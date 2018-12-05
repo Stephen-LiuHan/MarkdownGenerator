2678c14e1cdfe5891ca853029f904d85049f15a1
Stephen-LiuHan <stephensugimoto@gmail.com>
2018-12-05 11:09:37 +0900

    fix encording_bug

--- Minutes/Minutes.py
+++ Minutes/Minutes.py
@@ -2,12 +2,13 @@ import datetime
 import string
 import os
 
- #print("{},{}".format(__doc__,__file__))
+#print("{},{}".format(__doc__,__file__))
 currnt_file = __file__
 #print("{}".format(currnt_file.rfind("\\")))
 currnt_folder = os.path.dirname(os.path.abspath(currnt_file))
-with open(os.path.join(currnt_folder,"template","BasicMinutes.md"),"r") as tmp_file:
-    with open("test.md",'w') as md:
-        now = datetime.datetime.minute
-        ss=tmp_file.read()
-        md.write(ss.format(now=now))
+with open(os.path.join(currnt_folder,"template","BasicMinutes.md"),"rt",encoding="UTF-8") as tmp_file:
+    ss=tmp_file.read()
+with open("test.md",'w',encoding="UTF-8") as md:
+    start = datetime.datetime.now()
+    end = start+datetime.timedelta(hours=2) 
+    md.write(ss.format(start=start.strftime("%Y %b %d (%a) %H:%M"),end=end.strftime("%H:%M")))
